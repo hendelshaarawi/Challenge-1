85546467e6a3e7d42e6a200f4f27c2a6c04cb542
hendelshaarawi <hendelshaarawi@mbp-van-hend.wireless.hhs.nl>
2018-03-07 20:29:00 +0100

    Vakken toegevoegd

--- js/test.js
+++ js/test.js
@@ -317,3 +317,44 @@ let countDown = new Date('Aug 21, 2018 00:00:00').getTime(),
 
     }, second)
 
+
+    //BUTTON
+
+    ////////////////////////////////
+// Highlight Buttons
+var $highlight = $('.highlight');
+
+function translate(obj) {
+  
+    var $this    = obj.addClass('active'),
+        width    = $this.width(),
+        margin   = $this.outerWidth(true) - $this.outerWidth(),
+        position = $this.offset().left + margin/2;
+
+    $highlight.css({
+        width: parseInt(width) + 'px',
+        transform: 'translate(' + parseInt(position) + 'px' + ')'
+    });
+}
+
+// add transition after page load
+$(window).on('load resize', function() {
+    
+    // wait a little bit to add transition so we don't see on page load
+    setTimeout(function() {
+        $highlight.css({
+            transition: 'all .5s'
+        });
+    }, 100);
+
+    // load highlight on active control
+  translate($('li:first-child'));
+});
+
+// switch to active control on click
+$('li:not(.highlight)').on('click', function() {
+  
+  $('li').removeClass('active');
+  translate($(this));
+});
+
